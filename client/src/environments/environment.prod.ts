dde1ddef7fc786e6d64249a16590cf34919cefed
Twoje Imię <github@bestups.net>
2025-01-24 12:44:06 +0100

    nie dziala dalej

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://projekt-zaliczeniowy-1r1q.onrender.com/' // URL Twojego backendu
+  apiUrl: 'https://projekt-zaliczeniowy-1r1q.onrender.com'
 };
